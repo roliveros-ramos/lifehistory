ca3ce62400898c842f4820d2af19e1ef076caf57
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2022-10-19 04:18:47 +0200

    Imports.

--- R/aaa-.R
+++ R/aaa-.R
@@ -1,2 +1,8 @@
-#' @importFrom lubridate date
+#' @importFrom lubridate date yday parse_date_time year years yday<- year<-
+NULL
+#' @importFrom calibrar calibrate
+NULL
+#' @importFrom fields image.plot
+NULL
+#' @importFrom foreach foreach %dopar%
 NULL
