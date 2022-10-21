803760d64681de90cc7ea5735d53e0b77bffdd07
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2022-10-21 11:19:07 +0200

    check dates are dates when creating length frequency object.

--- R/lifehistory-main.R
+++ R/lifehistory-main.R
@@ -4,6 +4,7 @@
 lf_create = function(x, marks, date, bins=NULL) {
 
   if(is.null(bins)) bins = .calculate_bins(marks)
+  date = as.Date(date)
   out = list(x=x, marks=marks, date=date, bins=bins)
   class(out) = "len_freq"
   return(out)
