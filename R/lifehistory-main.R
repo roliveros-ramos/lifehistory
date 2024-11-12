5804c420825ef614630454f5528a8c39bac6f1a2
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2024-11-12 18:43:50 +0100

    main

--- R/lifehistory-main.R
+++ R/lifehistory-main.R
@@ -5,7 +5,14 @@ lf_create = function(x, marks, date, bins=NULL) {
 
   if(is.null(bins)) bins = .calculate_bins(marks)
   date = as.Date(date)
-  out = list(x=x, marks=marks, date=date, bins=bins)
+  if(nrow(x)!=length(marks)) stop("Length-frequency date dimension does not match number of marks.")
+  if(ncol(x)!=length(date)) stop("Length-frequency date dimension does not match number of dates.")
+
+  x = x[, order(date)] # sorted dates
+
+  to_keep = apply(x, 2, FUN=function(x) !all(x==0) | all(is.na(x)))
+
+  out = list(x=x[, to_keep], marks=marks, date=date[to_keep], bins=bins)
   class(out) = "len_freq"
   return(out)
 
