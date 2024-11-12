6cbcfde44d51e4ae5c6f01771ac0b5468d776a49
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2024-11-12 18:42:24 +0100

    imports

--- R/aaa-.R
+++ R/aaa-.R
@@ -1,12 +1,14 @@
-#' @importFrom lubridate date yday parse_date_time year years yday<- year<-
+#' @importFrom lubridate date yday parse_date_time year years yday<- year<- dmy
 NULL
 #' @importFrom calibrar calibrate
 NULL
 #' @importFrom fields image.plot
 NULL
-#' @importFrom foreach foreach %dopar%
+#' @importFrom foreach foreach %dopar% registerDoSEQ
 NULL
-#' @importFrom parallel detectCores makeCluster
+#' @importFrom parallel detectCores makeCluster stopCluster
 NULL
 #' @importFrom doSNOW registerDoSNOW
+NULL
+#' @importFrom gts interpolate
 NULL
